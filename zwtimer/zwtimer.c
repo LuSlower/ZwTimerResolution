fbf234f7a7daa199aff1afe979ee90efe8c6976e
SirSlower <148411728+LuSlower@users.noreply.github.com>
2025-03-31 12:51:13 -0500

    Add files via upload

--- zwtimer/zwtimer.c
+++ zwtimer/zwtimer.c
@@ -4,66 +4,47 @@ int main(int argc, char *argv[])
 {
 
     if (argc < 2){
-
-        // get frequency
         get_frq();
-
-        //loop
         loop_test();
     }
 
-    //help
-    if(strcmp(argv[1], "help") == 0 || strcmp(argv[1], "/?") == 0)
+    if (argc > 1 && (strcmp(argv[1], "--help") == 0 || strcmp(argv[1], "/?") == 0))
     {
-        printf("Instructions for use:\n");
-        printf("\n<resolution>\n");
-        printf("\nSet a new timer resolution (in hns units).\n");
-        printf("example: 'zwt.exe 5000'\n");
-        printf("\n<query>\n");
-        printf("\nReturns information about the timer resolution.\n");
-        printf("\n<test> <count>\n");
-        printf("\nGenerate a test on the precision of Sleep(1).\n");
-        printf("by default it runs in a loop\n");
-        printf("\n<stop>\n");
-        printf("\nStops all instances.\n");
+        printf("Usage:\n");
+        printf("  zwt.exe --res <value>      Set a new timer resolution (in hns units).\n");
+        printf("  zwt.exe --query            Returns information about the timer resolution.\n");
+        printf("  zwt.exe --test <count>     Generate a test on the precision of Sleep(1).\n");
+        printf("  zwt.exe --stop             Stops all instances.\n");
         return 0;
     }
 
-    if (strcmp(argv[1], "query") == 0)
+    if (strcmp(argv[1], "--query") == 0)
     {
         ZwQueryTimerResolution(&min, &max, &current);
-
-        // Obtener resoluciones del temporizador
-        printf("\nminimum resolution : %d hns\n", min);
+        printf("minimum resolution : %d hns\n", min);
         printf("maximum resolution : %d hns\n", max);
-        printf("current resolution : %d hns\n", current);
+        printf("current resolution : %d hns", current);
         return 0;
     }
 
-    //test de precision
-    if(strcmp(argv[1], "test") == 0 && argc == 3)
+    if(strcmp(argv[1], "--test") == 0 && argc == 3)
     {
         int count = atoi(argv[2]);
 
-        // verificar que count sea un dígito
         if (!isdigit(*argv[2])) {
             printf("Count must be a single digit.");
             return 1;
         }
 
-        // get frequency
         get_frq();
 
-        // declarar limites
         double min_sleep = INT_MAX, min_delta = INT_MAX, max_sleep = INT_MIN, max_delta = INT_MIN;
         double sum_sleep = 0, sum_delta = 0, sum_squared_sleep = 0, sum_squared_delta = 0;
 
-        //ejecutar bucle
         for (int i = 0; i < count; ++i)
         {
-            // Realizar una medición
             ZwQueryTimerResolution(&min, &max, &res_act);
-            Sleep(100);
+            Sleep(50);
             sleep_test();
 
             sum_delta += delta;
@@ -87,13 +68,11 @@ int main(int argc, char *argv[])
             if (delta > max_delta) {
                 max_delta = delta;
             }
-            printf("sleep(1): %.4f (delta: %.4f) | zwres: %d hns\n", tsleep, delta, res_act);
+            printf("zwres: %d hns, sleep(1): %.4f ms (delta: %.4f)\n", res_act, tsleep, delta);
         }
 
-        // Calcular promedio
         double avg_sleep = sum_sleep / count;
 
-        // Calcular desviación estándar
         double stdev_sleep = sqrt((sum_squared_sleep / count) - (avg_sleep * avg_sleep));
 
         printf("\nmin: %.4f ms", min_sleep);
@@ -103,66 +82,41 @@ int main(int argc, char *argv[])
 
         return 0;
     }
-    else if (strcmp(argv[1], "test") == 0)
+    else if (strcmp(argv[1], "--test") == 0)
     {
-
-        // get frequency
         get_frq();
-
-        //default
         loop_test();
     }
 
-    //detener instancias
-
-    if (strcmp(argv[1], "stop") == 0)
+    if (strcmp(argv[1], "--stop") == 0)
     {
         printf("stopped instances");
-        system("taskkill -f -im zwtimer.exe >nul");
+        system("taskkill -f -im zwtimer.exe >nul 2>&1");
         return 0;
     }
 
-       //set zwresolucion
-
-    // Verificar si argv[1] es un número entero
-    for (int i = 0; argv[1][i] != '\0'; i++)
+if (argc == 3 && strcmp(argv[1], "--res") == 0)
     {
-    if (!isdigit(argv[1][i]))
-        {
-            printf("/? or help to get help");
-            return 1;
-        }
-    }
+        char *endptr;
+        long res = strtol(argv[2], &endptr, 10);
 
-    //Verificar tamaño del argumento
-    if (strlen(argv[1]) >= 1 && strlen(argv[1]) < 6)
-    {
+        ULONG res_new;
+        ZwSetTimerResolution((ULONG)res, TRUE, &res_new);
+        Sleep(100);
+        printf("Resolution set correctly to %d hns\n", res_new);
 
-            res = atoi(argv[1]); // Convertir el argumento a ULONG
-            ZwSetTimerResolution(res, TRUE, &res_new); // Establecer resolución del temporizador
-            Sleep(100);
-            printf("resolution set correctly to %d hns", res_new);
+        SetPriorityClass(GetCurrentProcess(), PROCESS_MODE_BACKGROUND_BEGIN);
 
-            //prioridad de segundo plano
-            SetPriorityClass(GetCurrentProcess(), PROCESS_MODE_BACKGROUND_BEGIN);
+        _SetProcessInformation();
+        FreeConsole();
+        SetProcessWorkingSetSize(GetCurrentProcess(), (SIZE_T)-1, (SIZE_T)-1);
 
-            //llamar a SetProcessInformation
-            _SetProcessInformation();
-
-            //liberar consola
-            FreeConsole();
-
-            //liberar memoria
-            SetProcessWorkingSetSize(GetCurrentProcess(), (SIZE_T) -1, (SIZE_T) -1);
-
-            hEvent = CreateEvent(NULL, TRUE, FALSE, NULL); // Manual reset event, inicialmente no señalizado
-
-            // Suspender el hilo hasta que el evento se señalice
-            WaitForSingleObject(hEvent, INFINITE);
+        hEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
+        WaitForSingleObject(hEvent, INFINITE);
     }
     else
     {
-        printf("/? or help to get help");
+        printf("Use --help or /? to get help\n");
         return 1;
     }
 
--- zwtimer/zwtimer.h
+++ zwtimer/zwtimer.h
@@ -6,21 +6,16 @@
 #include <math.h>
 #include <tchar.h>
 
-// Definir valores
 ULONG min, max, current, res_act, res, res_new;
 
-// Definir contadores
 LARGE_INTEGER frq, start, end;
 
-// Definir semaforo, evento nulo
 HANDLE hMutex, hEvent;
 
-// Declaración de funciones Zw = Nt
 typedef LONG NTSTATUS;
 NTSYSAPI NTSTATUS NTAPI ZwSetTimerResolution(ULONG DesiredResolution, BOOLEAN SetResolution, ULONG *CurrentResolution);
 NTSYSAPI NTSTATUS NTAPI ZwQueryTimerResolution(ULONG *MinimumResolution, ULONG *MaximumResolution, ULONG *CurrentResolution);
 
-// Declaración de SetProcessInformation
 WINBASEAPI WINBOOL WINAPI SetProcessInformation(HANDLE hProcess, PROCESS_INFORMATION_CLASS ProcessInformationClass, LPVOID ProcessInformation, DWORD ProcessInformationSize);
 
 void _SetProcessInformation()
@@ -28,14 +23,12 @@ void _SetProcessInformation()
     if (SetProcessInformation)
     {
 
-        // Pasa un puntero a la estructura
         PROCESS_POWER_THROTTLING_STATE state;
         ZeroMemory(&state, sizeof(state));
         state.Version = PROCESS_POWER_THROTTLING_CURRENT_VERSION;
         state.ControlMask = PROCESS_POWER_THROTTLING_IGNORE_TIMER_RESOLUTION;
         state.StateMask = 0;
 
-        // Deshabilitar la resolución del temporizador de inactividad
         SetProcessInformation(GetCurrentProcess(), ProcessPowerThrottling, &state, sizeof(state));
     }
     else
@@ -44,7 +37,6 @@ void _SetProcessInformation()
     }
 }
 
-// declarar medidas
 double time, tsleep, delta;
 
 void sleep_test()
@@ -53,7 +45,6 @@ void sleep_test()
     Sleep(1);
     QueryPerformanceCounter(&end);
 
-    // Convertir la diferencia a milisegundos y restar el sleep(1)
     time = (double)(end.QuadPart - start.QuadPart) / frq.QuadPart;
     tsleep = time * 1000;
     delta = tsleep - 1;
@@ -61,21 +52,17 @@ void sleep_test()
 
 void loop_test()
 {
-    //bucle predeterminado
     for (int i = 1; ; i++) {
     ZwQueryTimerResolution(&min, &max, &res_act);
     sleep_test();
-    printf("sleep(1): %.4f (delta: %.4f) | zwres: %d hns\n", tsleep, delta, res_act);
-    Sleep(500);
+    printf("zwres: %d hns, sleep(1): %.4f ms (delta: %.4f)\n", res_act, tsleep, delta);
+    Sleep(250);
     }
 }
 
 void get_frq()
 {
-    // Obtener la frecuencia del contador de rendimiento establecer prioridad
     QueryPerformanceFrequency(&frq);
-
-    // 16 + 15 = 31, la prioridad más alta
     SetPriorityClass(GetCurrentProcess(), REALTIME_PRIORITY_CLASS);
     SetThreadPriority(GetCurrentThread(), THREAD_PRIORITY_TIME_CRITICAL);
 }
